a174aa192d68c4662d5ca5042cfd4c80414712ef
xphe <xphe@amarsoft.com>
2020-05-11 14:48:06 +0800

    添加实体类

---
+++ ems-parameter/src/main/java/com/amarsoft/app/ems/parameter/entity/LableCatalog.java
@@ -0,0 +1,121 @@
+/*
+ * 文件名：LableCatalog
+ * 版权：Copyright by www.amarsoft.com
+ * 描述：
+ * 修改人：xphe
+ * 修改时间：2020年5月11日
+ * 跟踪单号：
+ * 修改单号：
+ * 修改内容：
+ */
+package com.amarsoft.app.ems.parameter.entity;
+
+import com.amarsoft.amps.arem.annotation.Description;
+import com.amarsoft.amps.arpe.annotation.EntityRelationShip;
+import com.amarsoft.amps.arpe.annotation.GeneratedKey;
+
+import javax.persistence.*;
+import com.amarsoft.amps.arem.annotation.Description;
+import com.amarsoft.amps.arpe.businessobject.BusinessObject;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+/**
+ * 〈标签目录实体类〉
+ * 
+ * @author xphe
+ * @version 2020年5月11日
+ * @see 
+ * @since
+ */
+@Getter
+@Setter
+@Description("标签目录表")
+@Entity
+@Table(
+    name="LABEL_CATALOG")
+@GeneratedKey(autoGenerateKey=true, allocationSize=1000)
+public class LableCatalog extends BusinessObject {
+    private static final long serialVersionUID = 1L;
+      
+    @Description("标签编号") 
+    @Id 
+    @Column(name = "serialNo", nullable=false,length=40) 
+    private String serialNo;
+      
+    @Description("标签名称") 
+    @Column(name = "labelName",length=80) 
+    private String labelName;
+      
+    @Description("标签码值") 
+    @Column(name = "codeNo",length=80) 
+    private String codeNo;
+      
+    @Description("标签状态") 
+    @Column(name = "labelStatus",length=10) 
+    private String labelStatus;
+      
+    @Description("所属目录") 
+    @Column(name = "belongCataLog",length=80) 
+    private String belongCataLog;
+      
+    @Description("所属大类") 
+    @Column(name = "rootNo",length=40) 
+    private String rootNo;
+      
+    @Description("所属父类") 
+    @Column(name = "parentNo",length=40) 
+    private String parentNo;
+      
+    @Description("适用要求类别") 
+    @Column(name = "abilityType",length=10) 
+    private String abilityType;
+      
+    @Description("标签说明") 
+    @Column(name = "labelDescribe", nullable=false,length=2000) 
+    private String labelDescribe;
+      
+    @Description("目录备注") 
+    @Column(name = "catalogRemark",length=2000) 
+    private String catalogRemark;
+      
+    @Description("录入人") 
+    @Column(name = "author",length=40) 
+    private String author;
+      
+    @Description("版本") 
+    @Column(name = "labelVersion",length=10) 
+    private String labelVersion;
+      
+    @Description("登记人") 
+    @Column(name = "inputUserId", nullable=false,length=40) 
+    private String inputUserId;
+      
+    @Description("登记时间") 
+    @Column(name = "inputTime",length=20) 
+    private LocalDateTime inputTime;
+      
+    @Description("登记机构  ") 
+    @Column(name = "inputOrgId",length=40) 
+    private String inputOrgId;
+      
+    @Description("更新人") 
+    @Column(name = "updateUserId",length=40) 
+    private String updateUserId;
+      
+    @Description("更新时间") 
+    @Column(name = "updateTime",length=20) 
+    private LocalDateTime updateTime;
+      
+    @Description("更新机构  ") 
+    @Column(name = "updateOrgId",length=40) 
+    private String updateOrgId;
+    
+    @Description("类型  ") 
+    @Column(name = "labelType",length=10) 
+    private String labelType;
+}
