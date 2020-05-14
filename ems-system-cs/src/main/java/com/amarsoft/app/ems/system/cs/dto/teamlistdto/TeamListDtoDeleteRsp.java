8af825527ce800efd5b10a04249555ca789020e7
hpli <hpli@amarsoft.com>
2020-05-14 14:25:50 +0800

    提交注释

--- ems-system-cs/src/main/java/com/amarsoft/app/ems/system/cs/dto/teamlistdto/TeamListDtoDeleteRsp.java
+++ ems-system-cs/src/main/java/com/amarsoft/app/ems/system/cs/dto/teamlistdto/TeamListDtoDeleteRsp.java
@@ -1,6 +1,12 @@
 /*
- * 文件名：TeamListDtoDeleteRsp.java 版权：Copyright by www.amarsoft.com 描述： 修改人：hpli 修改时间：2020年5月13日
- * 跟踪单号： 修改单号： 修改内容：
+ * 文件名：TeamListDtoDeleteRsp.java 
+ * 版权：Copyright by www.amarsoft.com 
+ * 描述：
+ *  修改人：hpli 
+ *  修改时间：2020年5月13日
+ * 跟踪单号： 
+ * 修改单号： 
+ * 修改内容：
  */
 
 package com.amarsoft.app.ems.system.cs.dto.teamlistdto;
--- ems-system-cs/src/main/java/com/amarsoft/app/ems/system/cs/dto/teamlistdto/TeamListDtoQueryRoleRsp.java
+++ ems-system-cs/src/main/java/com/amarsoft/app/ems/system/cs/dto/teamlistdto/TeamListDtoQueryRoleRsp.java
@@ -1,6 +1,11 @@
 /*
- * 文件名：TeamListDtoQueryRoleRsp.java 版权：Copyright by www.amarsoft.com 描述： 修改人：hpli
- * 修改时间：2020年5月13日 跟踪单号： 修改单号： 修改内容：团队负责人查询
+ * 文件名：TeamListDtoQueryRoleRsp.java 
+ * 版权：Copyright by www.amarsoft.com 
+ * 描述：团队负责人查询
+ *  修改人：hpli
+ * 修改时间：2020年5月13日 
+ * 跟踪单号： 修改单号： 
+ * 修改内容：团队负责人查询
  */
 
 package com.amarsoft.app.ems.system.cs.dto.teamlistdto;
