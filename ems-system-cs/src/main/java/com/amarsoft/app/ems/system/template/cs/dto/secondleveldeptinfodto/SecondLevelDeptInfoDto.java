0e4e5303dc25be7a93e471ad3d89241f82730002
zcluo <zcluo@amarsoft.com>
2020-05-19 08:37:28 +0800

    修改dto模板

--- ems-system-cs/src/main/java/com/amarsoft/app/ems/system/template/cs/dto/secondleveldeptinfodto/SecondLevelDeptInfoDto.java
+++ ems-system-cs/src/main/java/com/amarsoft/app/ems/system/template/cs/dto/secondleveldeptinfodto/SecondLevelDeptInfoDto.java
@@ -49,7 +49,7 @@ public class SecondLevelDeptInfoDto implements Serializable {
     @Description("部门经理")
     @Length(max=40)
     @ActualColumn("DT.deptManagerName")
-    @TemplateBody(sortNo = 4, suffix = "", alignType = com.amarsoft.aecd.common.constant.TemplateAlignType.Left, editType = com.amarsoft.aecd.common.constant.TemplateEditType.Select, htmlStyle = "", isVisible = true, isReadOnly = false, span = 1, groupIndex = -1)
+    @TemplateBody(sortNo = 4, suffix = "", alignType = com.amarsoft.aecd.common.constant.TemplateAlignType.Left, editType = com.amarsoft.aecd.common.constant.TemplateEditType.Text, htmlStyle = "", isVisible = true, isReadOnly = false, span = 1, groupIndex = -1)
     private String deptManagerName;
 
     @Description("部门办公位置")
