099ec6b4c046272c109ac49fc86843614ae23fe3
dxiao <dxiao@amarsoft.com>
2020-05-19 14:58:33 +0800

    新增职级标签关联rep,res,dto

--- ems-employee-cs/src/main/java/com/amarsoft/app/ems/employee/template/cs/dto/employeerankrelabellistdto/EmployeeRankRelabelListDtoQueryRsp.java
+++ ems-employee-cs/src/main/java/com/amarsoft/app/ems/employee/template/cs/dto/employeerankrelabellistdto/EmployeeRankRelabelListDtoQueryRsp.java
@@ -1,4 +1,13 @@
-package com.amarsoft.app.ems.employee.template.cs.dto.employeerankinfodto;
+/*文件名：EmployeeRankRelabelListDtoQueryRsp
+ * 版权：Copyright by www.amarsoft.com
+ * 描述：查询职级标签的响应体
+ * 修改人：dxiao
+ * 修改时间：2020/05/19
+ * 跟踪单号：
+ * 修改单号：
+ * 修改内容：新生成
+ */
+package com.amarsoft.app.ems.employee.template.cs.dto.employeerankrelabellistdto;
 
 import java.io.Serializable;
 import lombok.Getter;
@@ -6,13 +15,13 @@ import lombok.Setter;
 import lombok.ToString;
 
 /**
- * 员工职级Info查询响应实体类
- * @author lding
+ * 员工职级标签查询响应实体类
+ * @author dxiao
  */
 @Getter
 @Setter
 @ToString
-public class EmployeeRankInfoDtoQueryRsp extends EmployeeRankInfoDto implements Serializable {
+public class EmployeeRankRelabelListDtoQueryRsp extends EmployeeRankRelabelListDto implements Serializable {
     private static final long serialVersionUID = 1L;
     
 }
