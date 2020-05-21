ecd32696bf449e245a98827381e2df7fe73f0ee6
xszhou <xszhou@amarsoft.com>
2020-05-21 20:02:14 +0800

    团队管理后端代码更新

--- ems-system-cs/src/main/java/com/amarsoft/app/ems/system/cs/dto/teamlistdto/TeamListDtoQueryReq.java
+++ ems-system-cs/src/main/java/com/amarsoft/app/ems/system/cs/dto/teamlistdto/TeamListDtoQueryReq.java
@@ -1,18 +1,25 @@
+/*
+ * 文件名：TeamListDtoQueryReq
+ * 版权：Copyright by www.amarsoft.com
+ * 描述：团队信息查询请求实体类
+ * 修改人：xszhou
+ * 修改时间：2020/5/21
+ * 跟踪单号：
+ * 修改单号：
+ * 修改内容：
+ */
+
 package com.amarsoft.app.ems.system.cs.dto.teamlistdto;
 
 import java.io.Serializable;
+import java.util.List;
+
+import com.amarsoft.amps.acsc.query.annotation.QueryRule;
+import com.amarsoft.amps.arem.annotation.Description;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import com.amarsoft.amps.acsc.query.annotation.QueryBegin;
-import com.amarsoft.amps.acsc.query.annotation.QueryOrderBy;
-import com.amarsoft.amps.acsc.query.annotation.QueryPageSize;
-import com.amarsoft.amps.acsc.query.annotation.QueryRule;
-import com.amarsoft.amps.acsc.query.QueryParameter;
-import com.amarsoft.amps.arem.annotation.Description;
-import com.amarsoft.amps.acsc.annotation.ActualColumn;
-import com.amarsoft.amps.acsc.annotation.NotEmpty;
-import com.amarsoft.amps.acsc.annotation.Range;
 
 /**
  * 团队信息查询请求实体类
@@ -24,28 +31,17 @@ import com.amarsoft.amps.acsc.annotation.Range;
 @QueryRule(groupBy = {})
 public class TeamListDtoQueryReq implements Serializable {
     private static final long serialVersionUID = 1L;
-    @Description("团队编号")
-    @ActualColumn("TINFO.teamId")
-    private QueryParameter<String> query_teamId;
-    @Description("起始条数")
-    @NotEmpty
-    @QueryBegin
+    
+    @Description("开始值")
     private Integer begin;
-    @Description("查询笔数")
-    @Range(min=1,max=10)
-    @NotEmpty
-    @QueryPageSize
+    
+    @Description("笔数")
     private Integer pageSize;
-    @Description("排序数组")
-    @QueryOrderBy
-    private String[] orderBy;
-    @Description("团队编号")
-    @ActualColumn("TINFO.teamName")
-    private String  teamName;
+    
     @Description("所属部门")
-    @ActualColumn("TINFO.belongOrgOId")
     private String  orgId;
-    @Description("角色A")
-    @ActualColumn("TINFO.roleA") 
-    private String  roleA;
+    
+    @Description("查询条件")
+    private List<Filter> filters; 
+
 }
