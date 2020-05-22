2c303152d9fad856db771b9ffd6d849b346c48a0
xszhou <xszhou@amarsoft.com>
2020-05-22 15:37:03 +0800

    修改团队部分保存逻辑（增加更新部分），删除无用接口

--- ems-system-cs/src/main/java/com/amarsoft/app/ems/system/cs/dto/teaminfodto/TeamInfoDtoQueryReq.java
+++ ems-system-cs/src/main/java/com/amarsoft/app/ems/system/cs/dto/teaminfodto/TeamInfoDtoQueryReq.java
@@ -1,15 +1,13 @@
 package com.amarsoft.app.ems.system.cs.dto.teaminfodto;
 
 import java.io.Serializable;
+
+import com.amarsoft.amps.acsc.query.annotation.QueryRule;
+import com.amarsoft.amps.arem.annotation.Description;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import com.amarsoft.amps.acsc.query.annotation.QueryRule;
-import com.amarsoft.amps.arem.annotation.Description;
-import com.amarsoft.amps.avta.annotation.TemplateBody;
-import com.amarsoft.amps.acsc.annotation.Length;
-import com.amarsoft.amps.acsc.annotation.NotEmpty;
-import com.amarsoft.amps.acsc.annotation.ActualColumn;
 
 /**
  * 团队信息查询请求实体类
@@ -18,22 +16,17 @@ import com.amarsoft.amps.acsc.annotation.ActualColumn;
 @Getter
 @Setter
 @ToString
-@QueryRule(groupBy = {})
 public class TeamInfoDtoQueryReq implements Serializable {
     private static final long serialVersionUID = 1L;
+    
     @Description("团队编号")
-    @Length(max=40)
-    @NotEmpty
-    @ActualColumn("TINFO.teamId")
     private String teamId;
+    
     @Description("所属部门")
-    @Length(max=40)
-    @ActualColumn("TINFO.belongOrgId")
-    @TemplateBody(sortNo = 5, suffix = "", alignType = com.amarsoft.aecd.common.constant.TemplateAlignType.Left, editType = com.amarsoft.aecd.common.constant.TemplateEditType.Text, htmlStyle = "", isVisible = true, isReadOnly = true, span = 1, groupIndex = -1)
     private String belongOrgId;
-    @Description("执行状态")
-    public String  status;
-   
+    
+    @Description("团队状态")
+    public String status; 
     
 }
 
